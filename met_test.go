50b7a6e0f9fff7062a4d9dd8c16cd68db42d6756
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-12-19 11:54:05 +0100

    add hello world

---
+++ met_test.go
@@ -0,0 +1,18 @@
+package met_test
+
+import (
+	"testing"
+
+	"github.com/fjukstad/met"
+)
+
+func TestHello(t *testing.T) {
+	text, err := met.SecureHello()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if text != "Hello to you too, securely!\n" {
+		t.Error(text)
+	}
+}
